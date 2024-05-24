a86f30b51481ebb3a09d93ec167cbc1c2efe3b9d
gemperador <gabriemperador@gmail.com>
2024-05-23 18:49:22 -0700

    working README generator

---
+++ Develop/README.md
@@ -0,0 +1,33 @@
+
+# README generator
+
+## Description
+Are you so lazy that you dont ever want to write your README? Here is an app to take care of that for you :)
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+Plug it in
+
+## Usage
+idk
+
+## License
+This project is licensed under the MIT license.
+
+## Contributing
+nun
+
+## Tests
+idk
+
+## Questions
+For any questions, you can contact me via:
+- GitHub: [gmoney](https://github.com/gmoney)
+- Email: gmoney@gmail.com
---
+++ Develop/index.js
@@ -0,0 +1,123 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What are the installation instructions?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'What is the usage information?',
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'What are the contribution guidelines?',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'What are the test instructions?',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your project:',
+      choices: ['MIT', 'GPLv2', 'Apache', 'GPLv3', 'BSD 3-Clause', 'None'],
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub username:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address:',
+    },
+  ];
+  
+module.exports = questions;
+  
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateReadmeContent(data), (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log(`README file '${fileName}' has been successfully created.`);
+    });
+}
+
+function generateReadmeContent(data) {
+    // Generate the content of the README file based on the user input data
+    return `
+# ${data.title}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+This project is licensed under the ${data.license} license.
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+For any questions, you can contact me via:
+- GitHub: [${data.github}](https://github.com/${data.github})
+- Email: ${data.email}
+`;
+}
+
+module.exports = writeToFile;
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+        .then((answers) => {
+            writeToFile('README.md', answers);
+        })
+        .catch((error) => {
+            console.error('An error occurred:', error);
+        });
+}
+
+module.exports = init;
+
+// Function call to initialize app
+init();
